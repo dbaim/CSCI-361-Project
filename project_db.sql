e830caccbe53bd16a6b53735e4075df1c0cee5fe
Dias Baimukhanov <79461183+dbaim@users.noreply.github.com>
2022-10-31 15:07:09 +0600

    Update project_db.sql

--- project_db.sql
+++ project_db.sql
@@ -1,30 +1,28 @@
-create database project;
-
-create table patients (
+CREATE TABLE patients (
     dob DATE,
-    iin CHAR(12),
+    iin CHAR(12) UNIQUE NOT NULL,
     patientId INT PRIMARY KEY,
-    full_name VARCHAR(255),
+    full_name VARCHAR(255) NOT NULL,
     blood_group VARCHAR(50),
     emergency_contact_number CHAR(12),
-    contact_number CHAR(12),
-    email VARCHAR(250),
+    contact_number CHAR(12) UNIQUE NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
     home_address VARCHAR(250),
     marital_status VARCHAR(250),
-    registration_date DATE,
-    username VARCHAR(50),
+    registration_date DATE NOT NULL,
+    username VARCHAR(50) REFERENCES authentification(username) NOT NULL,
     doctorId INT
 );
+
 CREATE TABLE doctor (
     dateOfBirth DATE,
-    iin CHAR(12),
-    doctorId INT PRIMARY KEY ,
-    patientID INT,
-    username VARCHAR(50),
-    
-    fullName VARCHAR(255),
-    phoneNumber CHAR(12),
-    departmentId INT,
+    iin CHAR(12) UNIQUE NOT NULL,
+    doctorId INT UNIQUE PRIMARY KEY,
+    patientID INT UNQUE NOT NULL,
+    username VARCHAR(50) REFERENCES authentification(username) NOT NULL,    
+    fullName VARCHAR(255) NOT NULL,
+    phoneNumber CHAR(12) UNIQUE NOT NULL,
+    departmentId INT NOT NULL,
     specializationsId INT,
     experience INT,
     category VARCHAR(50),
@@ -32,12 +30,12 @@ CREATE TABLE doctor (
     scheduleDetails VARCHAR(255),
     education VARCHAR(50),
     rating INT,
-    address VARCHAR(50),
-    email VARCHAR(250)
+    address VARCHAR(50) NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL
 );
 
 CREATE TABLE authentification (
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(50),
-    userType VARCHAR(20)
+    username VARCHAR(50) UNIQUE PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    userType VARCHAR(20) NOT NULL
 );
