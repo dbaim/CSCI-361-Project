c3ea5f84bd885bfe7e11fad9bd5f2b1acc68e10f
Dias Baimukhanov <79461183+dbaim@users.noreply.github.com>
2022-10-31 14:10:35 +0600

    Add files via upload
    Database credentials

---
+++ config.py
@@ -0,0 +1,6 @@
+hostname = 'ec2-52-23-131-232.compute-1.amazonaws.com'
+database = 'd5nvaf33pjv4t7'
+port_id = 5432
+username = 'keowogkklnprsn'
+pwd = '298a923ef358df5e6f22f3c3356c0015aa8d28a5abe869ee053e932019534b4a'
+url = 'postgres://keowogkklnprsn:298a923ef358df5e6f22f3c3356c0015aa8d28a5abe869ee053e932019534b4a@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d5nvaf33pjv4t7'
