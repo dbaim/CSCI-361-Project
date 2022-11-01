f5e0443e0891b841e88ae19fc9ae20c66aabea98
Dias Baimukhanov <79461183+dbaim@users.noreply.github.com>
2022-11-01 20:50:32 +0600

    Update views.py

--- admin_page/views.py
+++ admin_page/views.py
@@ -13,3 +13,26 @@ def login(request):
 
 def about(request):
     return HttpResponse("<h4>The hospital management system has a goal of providing a basic functionality for the patients and the hospital staff (doctors, nurses, managers, administration) to automate the processes of making an appointment, maintaining patient history, and prescribing medications and treatment procedures.</h4>")
+
+
+# def loginaction(request):
+#     global em, pwd
+#     if request.method == "POST":
+#         m = sql.connect(host="localhost", user="root", passwd="vivek", database='website')
+#         cursor = m.cursor()
+#         d = request.POST
+#         for key, value in d.items():
+#             if key == "email":
+#                 em = value
+#             if key == "password":
+#                 pwd = value
+#
+#         c = "select * from users where email='{}' and password='{}'".format(em, pwd)
+#         cursor.execute(c)
+#         t = tuple(cursor.fetchall())
+#         if t == ():
+#             return render(request, 'error.html')
+#         else:
+#             return render(request, "welcome.html")
+#
+#     return render(request, 'login_page.html')
