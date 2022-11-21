1d242b7ca47344839a18268718adf229f450ad29
dbaim <79461183+dbaim@users.noreply.github.com>
2022-11-21 13:26:23 +0600

    First commit

---
+++ SE project/djangoapp/djangoapp/urls.py
@@ -0,0 +1,34 @@
+"""djangoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('admin_page',views.showDoct, name = "showDoct"),
+    path('Insert', views.insertion, name="insertion"),
+    path('edit/<int:id>', views.update, name = "update"),
+    path('update/<int:id>', views.updateTable,name ="updateTable"),
+    path('delete/<int:id>', views.deleteRecord,name ="deleteRecord"),    
+    path('',views.admin_welcome, name = "welcome"),
+    path('InsertPat', views.insertionPat, name="insertionPat"),
+    path('deletePat/<int:id>', views.deletePatRecord,name ="deletePatRecord"),   
+    path('editPat/<int:id>', views.updatePat, name = "updatePat"),
+    path('updatePat/<int:id>', views.updateTablePat,name ="updateTablePat"), 
+    path('login', views.login, name = "login")
+]
