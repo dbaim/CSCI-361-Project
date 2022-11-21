7932e9f5fe9b30b8ed0fb45fa90806834aa9a832
Dias Baimukhanov <79461183+dbaim@users.noreply.github.com>
2022-11-21 17:48:36 +0600

    Update project_db.sql

--- project_db.sql
+++ project_db.sql
@@ -28,10 +28,10 @@ CREATE TABLE patients (
 CREATE TABLE doctor (
     dateOfBirth DATE,
     iin CHAR(12) UNIQUE NOT NULL,
-    doctorId INT UNIQUE PRIMARY KEY,
+    doctorId INT UNIQUE,
     patientID INT UNIQUE NOT NULL,
     username VARCHAR(50) REFERENCES authentification(username) NOT NULL,    
-    fullName VARCHAR(255) NOT NULL,
+    fullName VARCHAR(255) UNIQUE NOT NULL,
     phoneNumber CHAR(12) UNIQUE NOT NULL,
     departmentId INT NOT NULL,
     specializationsId INT,
@@ -42,16 +42,17 @@ CREATE TABLE doctor (
     education VARCHAR(50),
     rating INT,
     address VARCHAR(50) NOT NULL,
-    email VARCHAR(250) UNIQUE NOT NULL
+    email VARCHAR(250) UNIQUE NOT NULL,
+	CONSTRAINT doc_key PRIMARY KEY (doctorid, fullname)
 );
 
 CREATE TABLE appointment (
-    id INT NOT NULL,
     type VARCHAR(50) NOT NULL,
     description VARCHAR(100),
     date DATE UNIQUE NOT NULL,
     doctor INT REFERENCES doctor(doctorId) NOT NULL,
     patient INT REFERENCES patients(patientId) NOT NULL,
+	doc_name VARCHAR(255) REFERENCES doctor(fullname) NOT NULL,
     available BOOLEAN NOT NULL,
     CONSTRAINT app_key PRIMARY KEY (date, doctor)
 );
